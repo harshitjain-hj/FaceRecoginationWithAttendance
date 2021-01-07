dfb67995bc9f5a69f06248d5a0ff46e097932cb5
Rahul Singh Yashona <rahulsinghyashona625@gmail.com>
2021-01-07 17:16:08 +0530

    commented

--- attendanceProject.py
+++ attendanceProject.py
@@ -5,19 +5,20 @@ import os
 from datetime import datetime
 
 # from PIL import ImageGrab
-
+#  Getting Images and create List
 path = 'ImagesAttendance'
 images = []
 classNames = []
 myList = os.listdir(path)
 print(myList)
+
+# Importin Images one by one
 for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
 print(classNames)
-
-
+# Encoding faces
 def findEncodings(images):
     encodeList = []
     for img in images:
@@ -26,57 +27,64 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
-encodeListKnown = findEncodings(images)
-print('Encoding Complete')
-
+#   Marking Attendance
 def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
+    with open('attendance.csv','r+') as f:
         myDataList = f.readlines()
         nameList = []
+        # fetch data from list one by one
         for line in myDataList:
             entry = line.split(',')
             nameList.append(entry[0])
+        #     Check whether name is in the list or not
         if name not in nameList:
             now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
 
-
+# markAttendance('Rahul Singh')
 # #### FOR CAPTURING SCREEN RATHER THAN WEBCAM
 # # def captureScreen(bbox=(300,300,690+300,530+300)):
 # #     capScr = np.array(ImageGrab.grab(bbox))
 # #     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
 # #     return capScr
 #
-# encodeListKnown = findEncodings(images)
-# print('Encoding Complete')
-#
-cap = cv2.VideoCapture(0)
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
 
+# Initializing webcam
+capture = cv2.VideoCapture(0)
+#
 while True:
-    success, img = cap.read()
-    # img = captureScreen()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    success, img = capture.read()
+    #img = captureScreen()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+#     Finding all faces in our current frame
+    facesCurrentFrame = face_recognition.face_locations(imgS)
+#     Encoding of current frame
+    encodeCurrentFrame = face_recognition.face_encodings(imgS,facesCurrentFrame)
 
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis)
-        matchIndex = np.argmin(faceDis)
-
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
+#     Finding matches from the current frame
+    for encodeFace,faceLocation in zip(encodeCurrentFrame,facesCurrentFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDistance = face_recognition.face_distance(encodeListKnown,encodeFace)
+        print(faceDistance)
+        matchingIndex = np.argmin(faceDistance)
+#       Display name on the Image
+        if matches[matchingIndex]:
+            name = classNames[matchingIndex].upper()
             print(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+#           Creating rectangle around faces
+            y1,x2,y2,x1 = faceLocation
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
 
-    cv2.imshow('Webcam', img)
+    # Show webcam screen
+    cv2.imshow('Webcam',img)
     cv2.waitKey(1)
--- basics.py
+++ basics.py
@@ -1,26 +1,29 @@
 import cv2
-import numpy as np
 import face_recognition
 
+#  Importing Image
 imgElon = face_recognition.load_image_file('ImagesBasic/Elon Musk.jpg')
-imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('ImagesBasic/Bill gates.jpg')
-imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+imgTest = face_recognition.load_image_file('ImagesBasic/Elon Test.jpg')
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
-faceLoc = face_recognition.face_locations(imgElon)[0]
+#  Locate face and encoding Face
+faceLocation = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
-cv2.rectangle(imgElon, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+cv2.rectangle(imgElon,(faceLocation[3],faceLocation[0]),(faceLocation[1],faceLocation[2]),(255,0,255),2)
 
-# print(face_recognition.face_locations(imgTest))
-faceLocTest = face_recognition.face_locations(imgTest)[0]
+faceLocationTest = face_recognition.face_locations(imgTest)[0]
 encodeTest = face_recognition.face_encodings(imgTest)[0]
-cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
-#
-results = face_recognition.compare_faces([encodeElon], encodeTest)
-faceDis = face_recognition.face_distance([encodeElon], encodeTest)
-print(results, faceDis)
-cv2.putText(imgTest, f'{results} {round(faceDis[0], 2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+cv2.rectangle(imgTest,(faceLocationTest[3],faceLocationTest[0]),(faceLocationTest[1],faceLocationTest[2]),(255,0,255),2)
 
-cv2.imshow('Elon Musk', imgElon)
-cv2.imshow('Elon Test', imgTest)
+#   comparing faces and finding distance b/w them
+results = face_recognition.compare_faces([encodeElon],encodeTest)
+faceDistance = face_recognition.face_distance([encodeElon],encodeTest)
+print(results,faceDistance)
+
+# Display results and Face distance On the Test Images
+cv2.putText(imgTest,f'{results} {round(faceDistance[0],2)}',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+ 
+cv2.imshow('Elon Musk',imgElon)
+cv2.imshow('Elon Test',imgTest)
 cv2.waitKey(0)
